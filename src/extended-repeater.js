397f434de1183ddecc7f942ef9cbe6e413f322ca
StanislavDevel <unfeel00@gmail.com>
2025-01-03 19:03:57 +0100

    51/32

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,16 +1,17 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3,
+ *
+ * repeater('STRING', {
+ * repeatTimes: 3,
  * separator: '**',
  * addition: 'PLUS',
  * additionRepeatTimes: 3,
@@ -19,9 +20,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function repeater(str, options) {
+    const {
+        repeatTimes = 1,
+        separator = '+',
+        addition = '',
+        additionRepeatTimes = 1,
+        additionSeparator = '|',
+    } = options;
+    let stringAdd = String(addition)
+    let insideArr = Array(additionRepeatTimes).fill(stringAdd).join(additionSeparator)
+    let outSide = Array(repeatTimes).fill(str + insideArr).join(separator)
+    return (outSide)
 
 }
 
 module.exports = {
-  repeater
+    repeater
 };
